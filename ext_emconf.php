cf9c3df8b29473704510342cac94e04fc483f916
Jan-Michael Loew <loew@punkt.de>
2018-12-17 14:57:51 +0100

    TASK: bump requirement versions to typo3 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,7 +26,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '4.2.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.7.99',
+            'typo3' => '8.7.0-9.5.99',
             'php' => '7.0.0-7.2.99',
         ],
         'conflicts' => [],
